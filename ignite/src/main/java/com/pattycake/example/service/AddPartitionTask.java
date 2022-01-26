2b519a2a3cf17f00e6d54d5bf3a5a858e3c25616
Pretty Patty <LetTheAwesomeBegin@gmail.com>
2022-01-25 20:25:59 -0500

    Sleuth example

--- ignite/src/main/java/com/pattycake/example/service/AddPartitionTask.java
+++ ignite/src/main/java/com/pattycake/example/service/AddPartitionTask.java
@@ -1,48 +1,98 @@
 package com.pattycake.example.service;
 
 import com.pattycake.example.config.ApacheIgniteCacheConfiguration;
+import com.pattycake.example.model.InputModel;
+import com.pattycake.example.tracing.SleuthIgniteSpan;
+import com.pattycake.example.tracing.SleuthTracingPropagatorGetter;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.StreamSupport;
+import javax.cache.Cache.Entry;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
-import org.apache.ignite.cache.affinity.Affinity;
 import org.apache.ignite.cache.query.QueryCursor;
 import org.apache.ignite.cache.query.ScanQuery;
-import org.apache.ignite.cluster.ClusterNode;
 import org.apache.ignite.lang.IgniteCallable;
 import org.apache.ignite.resources.IgniteInstanceResource;
-
-import javax.cache.Cache;
-import java.util.Optional;
-import java.util.stream.StreamSupport;
+import org.apache.ignite.resources.SpringResource;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.cloud.sleuth.docs.AssertingSpanBuilder;
+import org.springframework.cloud.sleuth.propagation.Propagator;
 
 @Slf4j
 public class AddPartitionTask implements IgniteCallable<Integer> {
 
+    private final int partition;
+    private final Map<String, String> spanMap;
+    private final Propagator.Getter<Map<String, String>> extractor;
+//    private final Span parentSpan;
+
+    private Tracer tracer;
+
+    private Propagator propagator;
+
     @IgniteInstanceResource
     private Ignite ignite;
 
-    private int partition;
-
-    public AddPartitionTask(int partition) {
+    public AddPartitionTask(final int partition, final Map<String, String> spanMap
+                            //, final Span parentSpan
+    ) {
         this.partition = partition;
+        this.spanMap = spanMap;
+//        this.parentSpan = parentSpan;
+        this.extractor = new SleuthTracingPropagatorGetter();
+    }
+
+    @SpringResource(resourceClass = Tracer.class)
+    public void setTracer(final Tracer tracer) {
+        this.tracer = Objects.requireNonNull(tracer);
+    }
+
+    @SpringResource(resourceClass = Propagator.class)
+    public void setPropagator(final Propagator propagator) {
+        this.propagator = Objects.requireNonNull(propagator);
     }
 
     @Override
     public Integer call() throws Exception {
-        ClusterNode clusterNode = ignite.cluster().localNode();
-        Affinity<Object> affinity = ignite.affinity(ApacheIgniteCacheConfiguration.CACHE_NAME);
-        int[] localPartitons = affinity.primaryPartitions(clusterNode);
 
-        log.info("Running for partition[{}], All local partitions[{}]", partition, localPartitons);
+        Span newSpan = null;
+        final Span.Builder spanBuilder = AssertingSpanBuilder.of(SleuthIgniteSpan.IGNITE_CONSUMER_SPAN, propagator.extract(spanMap, extractor)
+                .kind(Span.Kind.CONSUMER))
+            .name("addPartitionTask");
 
-        IgniteCache<String, Integer> cache = ignite.cache(ApacheIgniteCacheConfiguration.CACHE_NAME);
-        try(QueryCursor<Cache.Entry<String, Integer>> cursor = cache.query(new ScanQuery<String, Integer>(partition).setLocal(true))) {
-            Optional<Integer> sum = StreamSupport.stream(cursor.spliterator(), false)
-                    .peek(entry -> log.info("key[{}] value[{}]", entry.getKey(), entry.getValue()))
-                    .map(entry -> entry.getValue())
-                    .reduce(Integer::sum);
+        final Span initialSpan = spanBuilder.start();
+        try (final Tracer.SpanInScope ws = this.tracer.withSpan(initialSpan)) {
+//        try (final Tracer.SpanInScope ws = this.tracer.withSpan(parentSpan)) {
+            newSpan = this.tracer.nextSpan().name("sum-partition-" + partition).tag("partition", Integer.toString(partition)).start();
+            return sum();
+        } finally {
+            // Once done remember to end the span. This will allow collecting
+            // the span to send it to e.g. Zipkin. The tags and events set on the
+            // newSpan will not be present on the parent
+            if (newSpan != null) {
+                newSpan.end();
+            }
+        }
+    }
 
+    @NotNull
+    private Integer sum() {
+        final IgniteCache<String, Integer> cache = ignite.cache(ApacheIgniteCacheConfiguration.CACHE_NAME);
+        try (final QueryCursor<Entry<String, InputModel>> cursor = cache.query(new ScanQuery<String, InputModel>(partition).setLocal(true))) {
+            final Optional<Integer> sum = StreamSupport.stream(cursor.spliterator(), false)
+//                .peek(entry -> log.info("key[{}] value[{}]", entry.getKey(), entry.getValue()))
+                .map(entry -> entry.getValue().getValue())
+                .reduce(Integer::sum);
+
+            log.info("Partition[{}] Sum[{}]", partition, sum);
             return sum.orElse(0);
         }
     }
+
+
 }
