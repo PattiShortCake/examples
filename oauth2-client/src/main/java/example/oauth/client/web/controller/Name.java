e3116e7a23d56b4afbb3b7a6ed9b4cd081183198
Patti Cake <28828310+PattiShortCake@users.noreply.github.com>
2022-08-19 22:40:47 -0400

    Add Java code generation from GraphQL

--- oauth2-client/src/main/java/example/oauth/client/web/controller/Name.java
+++ oauth2-client/src/main/java/example/oauth/client/web/controller/Name.java
@@ -1,16 +1,5 @@
 package example.oauth.client.web.controller;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.extern.jackson.Jacksonized;
-
-@Data
-@Jacksonized
-@AllArgsConstructor
-public class Name {
-
-  private final long id;
-  private final String firstName;
-  private final String lastName;
+public record Name(long id, String firstName, String lastName) {
 
 }
